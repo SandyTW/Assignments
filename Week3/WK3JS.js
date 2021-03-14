4209383be2347e49e7df3cc391d33ecce5940834
chaohsuan <chaohsuan@chaohsuans-MacBook.local>
2021-03-14 17:39:24 +0800

    20210314v1

--- Week3/WK3JS.js
+++ Week3/WK3JS.js
@@ -1,73 +1,48 @@
-
+let pictureAmount = 8;
+let totalPicture;
 const src =
-'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
+  'https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
 
 fetch(src)
-.then(function (response) {
-  return response.json();
-})
-.then((data) => {
-  dataProceeding(data);
-});
+  .then(function (response) {
+    return response.json();
+  })
+  .then((rawData) => {
+    dataProceeding(rawData);
+    totalPicture = rawData;
+  });
 
 function dataProceeding(data) {
-console.log(data);
-for (i = 0; i <8; i++) {
-  if (i % 4 === 0) {
-    let container = document.createElement('div');
-    container.classList.add('content-a');
-    container.setAttribute('id', 'content' + i);
-    document.body.appendChild(container);
-  };
-  let j = i - (i % 4);
-  let containerNew = document.getElementById('content' + j);
-  let n_topic = document.createElement('div');
-  let n_stitle = document.createElement('div');
-  let n_file = document.createElement('img');
-  
-  n_topic.appendChild(n_file);
-  n_topic.appendChild(n_stitle);
-  containerNew.appendChild(n_topic);
+  for (i = pictureAmount - 8; i < pictureAmount; i++) {
+    if (i % 4 === 0) {
+      let container = document.createElement('div');
+      container.classList.add('content-a');
+      container.setAttribute('id', 'content' + i);
+      document.getElementById('center').appendChild(container);
+    }
+    let j = i - (i % 4);
+    let containerNew = document.getElementById('content' + j);
+    let n_topic = document.createElement('div');
+    let n_stitle = document.createElement('div');
+    let n_file = document.createElement('img');
 
-  n_topic.classList.add('box');
-  n_stitle.classList.add('pic-name');
-  n_file.classList.add('image');
+    n_topic.appendChild(n_file);
+    n_topic.appendChild(n_stitle);
+    containerNew.appendChild(n_topic);
 
-  n_stitle.textContent = data['result']['results'][i].stitle;
-  n_file.src = 'http' + data['result']['results'][i].file.split('http')[1];
-  console.log(n_topic);
-}
+    n_topic.classList.add('box');
+    n_stitle.classList.add('pic-name');
+    n_file.classList.add('image');
+
+    n_stitle.textContent = data['result']['results'][i].stitle;
+    n_file.src = 'http' + data['result']['results'][i].file.split('http')[1];
+  }
 }
 
-// const src='https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json';
-// fetch(src).then(function (response) {
-//     return response.json();
-// }).then((result)=>{dataProceeding(result)})
+let btn = document.getElementById('btn-add');
+function handler() {
+  pictureAmount += 8;
+  dataProceeding(totalPicture);
+}
 
-
-// function dataProceeding(result) {
-//     let container = document.createElement("div");
-//     container.classList.add('content-a');
-//     document.body.appendChild(container);
-
-//     for(i=0; i<8; i++){
-//         let n_topic = document.createElement("div");
-//         let n_stitle = document.createElement("div");
-//         let n_file = document.createElement("img");
-
-//         n_topic.appendChild(n_file);
-//         n_topic.appendChild(n_stitle);
-//         container.appendChild(n_topic);
-        
-//         n_topic.classList.add('box')
-//         n_stitle.classList.add('pic-name');
-//         n_file.classList.add('image');
-        
-       
-//         n_stitle.textContent = result["result"]["results"][i].stitle;
-//         n_file.src = "http"+((result["result"]["results"][i].file).split('http'))[1];
-//         console.log(n_topic);
-
-//     }
-    
-// }
+btn.addEventListener('click', handler);
