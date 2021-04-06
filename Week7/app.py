6c0ef92764c13c07b693a054ce018170fe7864e2
chaohsuan <chaohsuan@chaohsuans-MacBook.local>
2021-04-06 21:43:13 +0800

    20210406v4

--- Week7/app.py
+++ Week7/app.py
@@ -13,7 +13,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Tj920419#!",
+  password="",
   database="website",
 )
 mycursor = mydb.cursor()
